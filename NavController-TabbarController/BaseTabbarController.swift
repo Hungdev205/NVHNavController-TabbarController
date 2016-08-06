7f40c577fcc4f5e4af50fa9c8baa93956ee99d68
imkooldev205 <hungnguyen@Hungs-MacBook-Pro.local>
2016-08-06 11:47:56 +0700

    Create ReadMe.md

---
+++ NavController-TabbarController/BaseTabbarController.swift
@@ -0,0 +1,42 @@
+//
+//  BaseTabbarController.swift
+//  NavController-TabbarController
+//
+//  Created by Vinh The on 7/17/16.
+//  Copyright © 2016 Vinh The. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabbarController: UITabBarController, UITabBarControllerDelegate {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        // 3 viewController dua vao TabbarController
+        let bookVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("YourBookVC") as! YourBookController
+        
+        let homeVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("HomeVC") as! HomeViewController
+        
+        let profileVC = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("ProfileVC") as! ProfileViewController
+        
+        // add 3 NavigationController
+        let bookNav = BaseNavigationController(rootViewController: bookVC)
+        let homeNav = BaseNavigationController(rootViewController: homeVC)
+        let profileNav = BaseNavigationController(rootViewController: profileVC)
+        
+        // Add cac viewController vao trong Tabbar:
+        viewControllers = [bookNav,homeNav,profileNav]
+        
+        profileVC.title = "Profile"
+        homeVC.title = "Home"
+        bookVC.title = "Book"
+    }
+    
+    
+}
